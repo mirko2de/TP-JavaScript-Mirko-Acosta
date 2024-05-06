f1a0e2d70a397952da64229bcbfd8b301532ec3f
Mirko Acosta <mirkoacosta55@gmail.com>
2024-05-06 11:02:01 -0300

    se creo el archivo para el trabajo de los numeros pares e impares

---
+++ assets/js/parimpar.js
@@ -0,0 +1,9 @@
+let num =parseInt(prompt("Ingrese un numero "));
+
+if(num % 2 === 0){
+    console.log("El numero ingresado es par")
+}else if(num % 2 !== 0){
+    console.log("El  numero ingresado no es par")
+}else{
+    console.log("Ingrese un valor valido")
+}
