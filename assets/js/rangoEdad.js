b03a0b1b70c6def0b161f488a7b1abcd0d5f015a
Mirko Acosta <mirkoacosta55@gmail.com>
2024-05-06 11:03:47 -0300

    se creo el archivo para el trabajo de rango de edad

---
+++ assets/js/rangoEdad.js
@@ -0,0 +1,9 @@
+if(edad => 0 && edad <= 12){
+    console.log("es un niño")
+}else if(edad => 13 && edad <=19){
+    console.log("es un adolescente")
+}else if(edad => 20 && edad <= 59){
+    console.log("es un adulto")
+}else if(edad > 60){
+    console.log("es un adulto mayor")
+}
